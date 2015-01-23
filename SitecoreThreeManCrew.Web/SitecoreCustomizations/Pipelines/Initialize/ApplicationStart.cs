325b101105e575aa974f83f626855ff562bc4090
goran.halvarsson <goranhalvarsson@gmail.com>
2015-01-23 16:26:36 +0100

    Creating a Sitecore 8 solution

---
+++ SitecoreThreeManCrew.Web/SitecoreCustomizations/Pipelines/Initialize/ApplicationStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Sitecore.Pipelines;
+using SitecoreThreeManCrew.Web.App_Start;
+
+namespace SitecoreThreeManCrew.Web.SitecoreCustomizations.Pipelines.Initialize
+{
+    public class ApplicationStart
+    {
+        public void Process(PipelineArgs args)
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
