0bb272a6a15cc614aac01b80be1ac1e0d5b12fe3
unknown <djanjicek@djanjicek-dell.hinttech.local>
2015-01-24 09:51:55 +0100

    Forgot this one

---
+++ ContentByMail/Pipelines/IEmailMessageProcessor.cs
@@ -0,0 +1,11 @@
+﻿namespace ContentByMail.Pipelines
+{
+    public interface IEmailMessageProcessor
+    {
+        /// <summary>
+        /// Processes the specified arguments.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        void Process(PostmarkMessageArgs args);
+    }
+}
