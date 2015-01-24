cfdf7f0d54909ddd3d67acbb8127eefb350cef1a
unknown <djanjicek@djanjicek-dell.hinttech.local>
2015-01-24 06:18:08 +0100

    Added new project

---
+++ ContentByMail/Pipelines/ItemWebApiRequest/PostmarkRequestProcessor.cs
@@ -0,0 +1,46 @@
+﻿namespace ContentByMail.Pipelines.ItemWebApiRequest
+{
+    using PostmarkDotNet;
+    using Sitecore.Diagnostics;
+    using Sitecore.ItemWebApi.Pipelines.Request;
+    using Sitecore.Pipelines;
+    using System;
+    using System.IO;
+    using System.Web;
+    using System.Web.Script.Serialization;
+
+    class PostmarkRequestProcessor : RequestProcessor
+    {
+        /// <summary>
+        /// Processes the specified arguments.
+        /// </summary>
+        /// <param name="arguments">The arguments.</param>
+        public override void Process(RequestArgs arguments)
+        {
+            Assert.ArgumentNotNull(arguments, "arguments");
+            HttpContext context = HttpContext.Current;
+
+            if (context == null || context.Request == null || context.Request.InputStream == null)
+                return;
+
+            context.Request.InputStream.Position = 0;
+            string json = null;
+
+            using (var inputStream = new StreamReader(context.Request.InputStream))
+            {
+                json = inputStream.ReadToEnd();
+            }
+
+            if (!String.IsNullOrEmpty(json))
+            {
+                JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+                PostmarkMessage message = javaScriptSerializer.Deserialize(json, typeof(PostmarkMessage)) as PostmarkMessage;
+
+                if (message != null)
+                {
+                    CorePipeline.Run("Hackathon.ProcessEmail", new PostmarkMessageArgs(message));
+                }
+            }
+        }
+    }
+}
---
+++ ContentByMail/Pipelines/PostmarkMessageArgs.cs
@@ -0,0 +1,22 @@
+﻿namespace ContentByMail.Pipelines
+{
+    using PostmarkDotNet;
+    using Sitecore.Pipelines;
+
+    public class PostmarkMessageArgs : PipelineArgs
+    {
+        /// <summary>
+        /// Gets or sets the message.
+        /// </summary>
+        public PostmarkMessage Message { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PostmarkMessageArgs"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public PostmarkMessageArgs(PostmarkMessage message)
+        {
+            this.Message = message;
+        }
+    }
+}
