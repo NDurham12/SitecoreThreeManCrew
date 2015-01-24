3544c77b38f6b617dcc68f75adb54c8be845130e
djanjicek <d.janjicek@gmail.com>
2015-01-24 13:45:22 +0100

    Work in progress

--- ContentByMail/Core/EmailProcessor/EmailProcessorTemplateToken.cs
+++ ContentByMail/Core/EmailProcessor/EmailProcessorTemplateToken.cs
@@ -1,12 +1,6 @@
 ﻿namespace ContentByMail.Core.EmailProcessor
 {
-<<<<<<< HEAD
     using ContentByMail.Common;
-    using ContentByMail.Core.Notifications;
-=======
-
-    using ContentByMail.Common;
->>>>>>> 1add5dcc08bc83a429eaa24e19e9667b5820438b
     using Sitecore.Data.Items;
     using Sitecore.Diagnostics;
 
@@ -22,7 +16,7 @@
             Assert.ArgumentNotNull(item, "EmailProcessorTemplate item is null");
             Assert.IsTrue(item.IsDerived(Constants.Templates.EmailProcessorTemplateToken), "Derives from wrong template");
 
-            Name = item.GetString(Constants.Fields.EmailProcessorTemplateTokens.EmailProcessorTemplateToken);
+            this.Name = item.GetString(Constants.Fields.EmailProcessorTemplateTokens.EmailProcessorTemplateToken);
         }
     }
 }
